adaa1712aa1b22b5c4bf9e459f90fbb2f0bf7d0c
Jeremy Rodriguez <jeremy.rodrguez@yahoo.com>
2020-11-02 02:35:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,7 @@ def find_valid_calls(planeteer_calls)
   
   if planeteer_calls.find_index(valid_calls)
     puts valid_calls
-  else planeteer_calls
-  end
+  else planeteer_calls.find_index(valid_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
 end
